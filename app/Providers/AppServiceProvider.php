24019e82a7229d1653ea288271916ac1fa400866
Zoltán Tamás Szabó <zoli.szabok@gmail.com>
2025-06-12 09:37:38 +0200

    feat: configure Filament components and update minimum stability in composer files

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use Filament\Forms\Components\FileUpload;
+use Filament\Infolists\Components\ImageEntry;
+use Filament\Tables\Columns\ImageColumn;
 use Illuminate\Support\ServiceProvider;
 
 final class AppServiceProvider extends ServiceProvider
@@ -21,6 +24,14 @@ final class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+
+        FileUpload::configureUsing(fn (FileUpload $fileUpload) => $fileUpload
+            ->visibility('public'));
+
+        ImageColumn::configureUsing(fn (ImageColumn $imageColumn) => $imageColumn
+            ->visibility('public'));
+
+        ImageEntry::configureUsing(fn (ImageEntry $imageEntry) => $imageEntry
+            ->visibility('public'));
     }
 }
