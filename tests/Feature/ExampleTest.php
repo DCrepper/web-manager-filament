5359fcb0b00d6fb5941aba9691eca6c0e198a961
Zoltán Tamás Szabó <zoli.szabok@gmail.com>
2025-06-12 09:20:03 +0200

    Install Pest

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,8 +1,6 @@
 <?php
 
-declare(strict_types=1);
-
-test('the application returns a successful response', function (): void {
+it('returns a successful response', function () {
     $response = $this->get('/');
 
     $response->assertStatus(200);
