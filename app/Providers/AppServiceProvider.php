1dd8ffd4d57c7c077b948c0af4c607de2f4e2d2e
Zoltán Tamás Szabó <zoli.szabok@gmail.com>
2025-06-13 15:37:21 +0200

    feat: Add configuration for Fieldset, Grid, and Section components to span full columns

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,9 @@ namespace App\Providers;
 
 use Filament\Forms\Components\FileUpload;
 use Filament\Infolists\Components\ImageEntry;
+use Filament\Schemas\Components\Fieldset;
+use Filament\Schemas\Components\Grid;
+use Filament\Schemas\Components\Section;
 use Filament\Tables\Columns\ImageColumn;
 use Illuminate\Support\ServiceProvider;
 
@@ -33,5 +36,14 @@ final class AppServiceProvider extends ServiceProvider
 
         ImageEntry::configureUsing(fn (ImageEntry $imageEntry): ImageEntry => $imageEntry
             ->visibility('public'));
+
+        Fieldset::configureUsing(fn (Fieldset $fieldset) => $fieldset
+            ->columnSpanFull());
+
+        Grid::configureUsing(fn (Grid $grid) => $grid
+            ->columnSpanFull());
+
+        Section::configureUsing(fn (Section $section) => $section
+            ->columnSpanFull());
     }
 }
